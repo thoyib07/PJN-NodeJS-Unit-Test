c4c2214b4459a96611b357f69c59698aa79149fd
Thoyib Hidayat <thoyib07@gmail.com>
2022-11-08 17:57:40 +0700

    jest configuration

--- test/sum.test.js
+++ test/sum.test.js
@@ -5,3 +5,15 @@ test('Test Sum Function', () => {
 
     expect(result).toBe(3);
 });
+
+test('Test Sum Function 2', () => { 
+    const result = sum(1,2);
+
+    expect(result).toBe(3);
+});
+
+test('Test Sum Function 3', () => { 
+    const result = sum(1,2);
+
+    expect(result).toBe(3);
+});
